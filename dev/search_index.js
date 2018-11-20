3f5b893cb5009e1f013fa5510752cb3bda8ddc4e
autodocs <autodocs>
2018-11-20 17:14:45 +0000

    build based on d8ccd9e

---
+++ dev/search_index.js
@@ -0,0 +1,83 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "#",
+    "page": "Home",
+    "title": "Home",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "#JuliaPackageTemplate.jl-1",
+    "page": "Home",
+    "title": "JuliaPackageTemplate.jl",
+    "category": "section",
+    "text": ""
+},
+
+{
+    "location": "api/#",
+    "page": "API",
+    "title": "API",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "api/#API-1",
+    "page": "API",
+    "title": "API",
+    "category": "section",
+    "text": ""
+},
+
+{
+    "location": "api/#Types-1",
+    "page": "API",
+    "title": "Types",
+    "category": "section",
+    "text": "Order = [:type]"
+},
+
+{
+    "location": "api/#Functions-1",
+    "page": "API",
+    "title": "Functions",
+    "category": "section",
+    "text": "Order = [:function]"
+},
+
+{
+    "location": "api/#JuliaPackageTemplate.foobar",
+    "page": "API",
+    "title": "JuliaPackageTemplate.foobar",
+    "category": "function",
+    "text": "foobar\n\nJust a dummy function.\n\n\n\n\n\n"
+},
+
+{
+    "location": "api/#Documentation-1",
+    "page": "API",
+    "title": "Documentation",
+    "category": "section",
+    "text": "Modules = [JuliaPackageTemplate]\nOrder = [:type, :function]"
+},
+
+{
+    "location": "LICENSE/#",
+    "page": "LICENSE",
+    "title": "LICENSE",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "LICENSE/#LICENSE-1",
+    "page": "LICENSE",
+    "title": "LICENSE",
+    "category": "section",
+    "text": "using Markdown\nMarkdown.parse_file(joinpath(@__DIR__, \"..\", \"..\", \"LICENSE.md\"))"
+},
+
+]}
